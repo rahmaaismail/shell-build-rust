5ba8f31211d69463c7b0f9d5d948587c7edc2b5c
Rahma Ismail <rahmaismail@uvic.ca>
2026-05-03 15:44:45 -0700

    implement a REPL: read-eval-print loop

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut command = String::new();
-
-    // Captures the user's command in the "command" variable
-    io::stdin().read_line(&mut command).unwrap();
-
-    // Prints the "<command>: command not found" message
-    println!("{}: command not found", command.trim())
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        // Wait for user input
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        println!("{}: command not found", command.trim());
+    }
 }
+
