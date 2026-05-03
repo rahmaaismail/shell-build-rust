80b2e0eda26f557e15b6259db264f5d3e5073d16
Rahma Ismail <rahmaismail@uvic.ca>
2026-05-03 16:44:02 -0700

    double quotes

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,24 @@ fn parse_args(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current = String::new();
     let mut in_single_quote = false;
+    let mut in_double_quote = false;
     let mut chars = input.chars().peekable();
 
     while let Some(c) = chars.next() {
         match c {
-            '\'' if !in_single_quote => {
+            '\'' if !in_single_quote && !in_double_quote => {
                 in_single_quote = true;
             }
             '\'' if in_single_quote => {
                 in_single_quote = false;
             }
-            ' ' | '\t' if !in_single_quote => {
+            '"' if !in_single_quote && !in_double_quote => {
+                in_double_quote = true;
+            }
+            '"' if in_double_quote => {
+                in_double_quote = false;
+            }
+            ' ' | '\t' if !in_single_quote && !in_double_quote => {
                 if !current.is_empty() {
                     args.push(current.clone());
                     current.clear();
