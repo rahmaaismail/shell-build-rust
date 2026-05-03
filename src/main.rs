25565aad78451a017df5f12c34a4f3e8b6464373
Rahma Ismail <rahmaismail@uvic.ca>
2026-05-03 16:12:21 -0700

    implement type

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,29 @@
 use std::io::{self, Write};
 
 fn main() {
+    let builtins = vec!["echo", "exit", "type"];
 
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         
-        // Wait for user input
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim().to_string();
-        // Exit the shell
+
         if command == "exit" {
             break;
         } else if command.starts_with("echo ") {
             println!("{}", &command[5..]);
+        } else if command.starts_with("type ") {
+            let arg = &command[5..];
+            if builtins.contains(&arg) {
+                println!("{} is a shell builtin", arg);
+            } else {
+                println!("{}: not found", arg);
+            }
         } else {
             println!("{}: command not found", command);
-                  }
         }
+    }
 }
-
